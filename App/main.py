dbea26a14b436af912795d96d7b1ade08d561b23
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-09-18 16:17:19 +0200

    EHN: use numba and jit

--- App/main.py
+++ App/main.py
@@ -1,19 +1,18 @@
-from skimage import io
-import matplotlib.pyplot as plt
-import numpy as np
-import DescGlob.fourrier1 as dgl
+import neurtu
+
+from skimage.io import imread
+from skimage.transform import resize
+
+from DescGlob.fourrier1 import fourrier1
 
 
-def main():
-    img = io.imread('lena128x128.jpg')
-    # io.imshow(img)
-    # io.show()
-    # print(img.shape)
-    desc_fourrier1 = dgl.fourrier1(img)
-    x = np.arange(0, len(desc_fourrier1), 1)
-    plt.plot(x, desc_fourrier1)
-    plt.show()
+def cases():
+    for size in [64, 128, 256, 512]:
+        tags = {'size': (size, size)}
+        img = imread('lena128x128.jpg')
+        img_resized = resize(img, (size, size))
+        yield neurtu.delayed(fourrier1, tags=tags)(img_resized)
 
-
-if __name__ == '__main__':
-    main()
+df = neurtu.timeit(cases())
+df['wall_time'] *= 1000
+print(df)
