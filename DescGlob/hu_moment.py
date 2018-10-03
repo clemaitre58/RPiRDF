5745ca3007f90d5c17c33cc9021181f097c72a26
Cédric Lemaitre <c.lemaitre58@gmail.com>
2018-10-03 16:59:36 +0200

    add color version for hu

--- DescGlob/hu_moment.py
+++ DescGlob/hu_moment.py
@@ -3,7 +3,7 @@ from skimage.measure import moments_hu
 
 
 def hu_moment(image):
-    """hu_moment descriptor.
+    """hu_moment descriptor for grayscale image.
 
     Parameters:
         image : ndarray, shape (height, width)
@@ -17,3 +17,23 @@ def hu_moment(image):
 
     M = moments_central(image, cr=int(r/2), cc=int(c/2), order=3)
     return moments_hu(M)
+
+
+def hu_moment_color(image_color):
+    """hu_moment descriptor for color image.
+
+    Parameters:
+        image : ndarray, shape (height, width, 3)
+            Input image.
+
+    Returns:
+        descriptor : ndarray, shape (21)
+            The Hu Momemnt for each color component.
+    """
+
+    M_r = hu_moment(image_color[:, :, 0])
+    M_g = hu_moment(image_color[:, :, 1])
+    M_b = hu_moment(image_color[:, :, 2])
+
+    M = M_r + M_g + M_b
+    return M
