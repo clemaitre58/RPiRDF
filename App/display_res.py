f5563709d94a47fc3804f9c065127672efc7db32
Cédric Lemaitre <c.lemaitre58@gmail.com>
2018-11-26 22:46:35 +0100

    dispay results

--- App/display_res.py
+++ App/display_res.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+df = pd.read_csv('data.csv')
+
 s_f = np.load('s_f.npy')
 s_h = np.load('s_h.npy')
 s_z = np.load('s_z.npy')
 
-f = s_f['test_score']
-h = s_h['test_score']
-z = s_z['test_score']
+f = s_f['f']
+h = s_h['h']
+z = s_z['z']
 
-d = {'f': f, 'h': h, 'z': z}
-df = pd.DataFrame(data=d)
 
 sns.set(style='whitegrid')
 ax = sns.boxplot(x='C_SVS', y='Test ', data=df, linewidth=2.5)
