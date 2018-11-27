669fcc8fdcc70db8e7b5a4513877e013a29e44cf
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-11-27 10:22:48 +0100

    make it beautiful

--- App/display_res.py
+++ App/display_res.py
@@ -4,16 +4,9 @@ import seaborn as sns
 
 df = pd.read_csv('test_score.csv', index_col=0)
 
-# s_f = np.load('s_f.npy')
-# s_h = np.load('s_h.npy')
-# s_z = np.load('s_z.npy')
-#
-# f = s_f['f']
-# h = s_h['h']
-# z = s_z['z']
-
-
-sns.set(style='whitegrid')
-# ax = sns.boxplot(x='C_SVS', y='Test ', data=df, linewidth=2.5)
-ax = sns.boxplot(data=df, linewidth=2.5)
-plt.show()
+with sns.plotting_context("poster"):
+    sns.boxplot(data=df, whis=100)
+    sns.despine(bottom=True)
+    plt.ylim([0.5, 1.0])
+    plt.yticks([0.5, 0.75, 1.0])
+    plt.show()
